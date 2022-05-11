9c787789cae51211fa8b0510310d1e40bb276683
dawidos234 <dawidos234@gmail.com>
2022-05-11 09:52:03 +0200

    beautify

--- screenshot.js
+++ screenshot.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer');
 (async () => {
-	const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(process.argv[2], {
         waitUntil: 'networkidle2',
     });
     await page.evaluate(() => {
-		let isAReply = 0;
-		if (window.scrollY > 0) isAReply = 1;
+        let isAReply = 0;
+        if (window.scrollY > 0) isAReply = 1;
         let tweet = document.querySelectorAll("article[data-testid=tweet]")[isAReply].parentNode;
         if (isAReply) {
             document.querySelectorAll("div.r-1bimlpy")[1].style.backgroundColor = "white";
